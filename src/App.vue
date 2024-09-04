60647734d2c6d23b0cfaca4314d9158827915d2a
HyeonJi Tak <jirae1997@naver.com>
2024-09-04 17:28:15 +0900

    feat: 데이터 바인딩과 for문 문법

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="menu">
+        <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+    </div>
+    <div>
+        <h4>{{ products[0] }}</h4>
+        <p>{{ price1 }} 만원</p>
+    </div>
+    <div>
+        <h4>{{ products[1] }}</h4>
+        <p>{{ price2 }} 만원</p>
+    </div>
+    <div>
+        <h4>{{ products[2] }}</h4>
+        <p>{{ price3 }} 만원</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            price1: 60,
+            price2: 70,
+            price3: 40,
+            menus: ["Home", "Products", "About"],
+            products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
+        };
+    },
+    components: {
+        // HelloWorld,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+.menu {
+    background: darkblue;
+    padding: 16px;
+    border-radius: 4px;
+}
+.menu a {
+    color: white;
+    padding: 10px;
 }
 </style>
