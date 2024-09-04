58fa92d7aa5131a79775dfbec83183d40578b282
HyeonJi Tak <jirae1997@naver.com>
2024-09-04 17:53:07 +0900

    feat: 이미지 추가와 모달 생성

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
+    <div class="black-bg" v-if="isOpenModal == true">
+        <div class="white-bg">
+            <h4>상세페이지임</h4>
+            <p>상세페이지 내용임</p>
+            <button @click="isOpenModal = false">닫기</button>
+        </div>
+    </div>
+
     <div class="menu">
         <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
     </div>
     <div>
-        <h4>{{ products[0] }}</h4>
+        <img src="./assets/images/room0.jpg" class="room-img" />
+        <h4 @click="isOpenModal = true">{{ products[0] }}</h4>
         <p>{{ price1 }} 만원</p>
-        <button @click="increase">허위매물 신고</button>
-        <span>신고수 : {{ 신고수 }}</span>
+        <button @click="increase(0)">허위매물 신고</button>
+        <span>신고수 : {{ 신고수[0] }}</span>
     </div>
     <div>
+        <img src="./assets/images/room1.jpg" class="room-img" />
         <h4>{{ products[1] }}</h4>
         <p>{{ price2 }} 만원</p>
+        <button @click="increase(1)">허위매물 신고</button>
+        <span>신고수 : {{ 신고수[1] }}</span>
     </div>
     <div>
+        <img src="./assets/images/room2.jpg" class="room-img" />
         <h4>{{ products[2] }}</h4>
         <p>{{ price3 }} 만원</p>
+        <button @click="increase(2)">허위매물 신고</button>
+        <span>신고수 : {{ 신고수[2] }}</span>
     </div>
 </template>
 
@@ -25,7 +40,8 @@ export default {
     name: "App",
     data() {
         return {
-            신고수: 0,
+            isOpenModal: false,
+            신고수: [0, 0, 0],
             price1: 60,
             price2: 70,
             price3: 40,
@@ -34,8 +50,8 @@ export default {
         };
     },
     methods: {
-        increase() {
-            this.신고수 += 1;
+        increase(i) {
+            this.신고수[i] += 1;
         },
     },
     components: {
@@ -45,6 +61,25 @@ export default {
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+div {
+    box-sizing: border-box;
+}
+.black-bg {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+}
+.white-bg {
+    width: 100%;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;
+}
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -60,5 +95,9 @@ export default {
 .menu a {
     color: white;
     padding: 10px;
+}
+.room-img {
+    width: 100%;
+    margin-top: 40px;
 }
 </style>
